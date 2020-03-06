25ae097b7bdcc67065572748f651c1626df40313
gilson-a <gilson.a@zeusdobrasil.com.br>
2020-03-06 17:53:04 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/ObjetDeValor/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool Ehboleto {
+            get { return Id == (int) TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EnDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
